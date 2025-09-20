f48b4749ca55d042fe13571dc400b39238b58402
Dmitriy Davletshin <dimka3007@gmail.com>
2025-09-20 08:28:14 +0300

    check flake8 4

--- src/database.py
+++ src/database.py
@@ -1,5 +1,10 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
 
 DATABASE_URL = "sqlite+aiosqlite:///./app.py.db"
 
